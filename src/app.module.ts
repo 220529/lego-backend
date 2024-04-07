a70f0a38a4b6846913d3aa9b77d864b955714e5e
liukaixin <15713857919@163.com>
2024-04-07 14:26:41 +0800

    feat: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
@@ -8,13 +9,20 @@ import { CatsModule } from './cat/cat.module';
 @Module({
   imports: [
     CatsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'production' ? '.env.prod' : '.env.dev',
+    }),
     MongooseModule.forRootAsync({
-      useFactory: () => {
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
         return {
-          user: 'root',
-          pass: 'root',
-          dbName: 'lego',
-          uri: 'mongodb://localhost:27017',
+          user: configService.get('ROOT_NAME'),
+          pass: configService.get('ROOT_PASSWORD'),
+          dbName: configService.get('DATABASE'),
+          uri: `mongodb://${configService.get('HOST')}:${configService.get('PORT')}`,
         };
       },
     }),
