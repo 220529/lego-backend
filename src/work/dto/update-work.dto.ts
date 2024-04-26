e2a221f9f1f69fb8d409b94e95f4ec6ff33bae29
liukaixin <15713857919@163.com>
2024-04-26 15:56:41 +0800

    fix: work

--- src/work/dto/update-work.dto.ts
+++ src/work/dto/update-work.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateWorkDto } from './create-Work.dto';
+import { CreateWorkDto } from './create-work.dto';
 
 export class UpdateWorkDto extends PartialType(CreateWorkDto) {}
